e43e519935546c6b237655f62b763f7521b606b0
Yuferev Kirill <yuferev.k@protonmail.ch>
2018-02-02 14:29:20 +0300

    development

--- bash/backup_rotate.sh
+++ bash/backup_rotate.sh
@@ -2,4 +2,20 @@
 # Universal backup rotation script
 # Author: Kirill Yuferev, kyuferev@mera.ru
 # Additional email: yuferev.k@protonmail.ch
-# Version: 0.1s
+# Version: 0.1
+##############
+# PARAMETERS #
+##############
+LOG_DIR=""
+##############################################
+# NO CHANGES SHOULD BE MADE BEYOND THIS LINE #
+##############################################
+DATE=`date +%F@%T | sed "s/://g"`
+DAY=`date +%A`
+
+#############
+# FUNCTIONS #
+#############
+function write_log() {
+	echo -en "$1" >> "$LOG_FILE"
+}
